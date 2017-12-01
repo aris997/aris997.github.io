9ef21d7ff45dc6f96eb1366312cdb372237bc4ba
Aris997 <riccardo.riva.997@gmail.com>
2017-12-01 15:53:01 +0100

    credo che dio sia porco

--- gas/gas.c
+++ gas/gas.c
@@ -4,12 +4,12 @@
 #include <math.h>
 #include <time.h>
 
-#define L 20
+#define L 80
+#define RHO 0.25
+#define MEASURES (TMAX/WIDTH)
 #define TMAX 2000
-#define NSTORIES 10
-#define RHO 0.20
 #define WIDTH 10
-#define MEASURES 1000
+#define NSTORIES 10
 
 //RODARI-RIVA   30 NOVEMBRE 2017    v1.0.0    lfc409
 
@@ -19,23 +19,22 @@
 //
 //
 
-typedef long long int position;
+typedef long long int pos;
 typedef unsigned long long int steps;
 
 typedef struct coord{
-  long int x;
-  long int y;
+  pos x;
+  pos y;
 } coord;
 
 void errori(int);
-void init(int [][L]);
-void fprinter(int [][L]);
-void initdouble(double [][MEASURES]);
+void init(pos [][L]);
+void fprinter(pos [][L]);
+void check_diocane(pos [][L]);
 void initNeighbor(long int*,long int*);
-//void Sites2Particels(coord*,int [][L],int);
-void seedingParticels(int [][L],coord*,coord*,coord*,int);
-void particelswalk(int [][L],coord*,coord*,long int*,long int*,int);
-void measurements(coord*,coord*,double*,double [][MEASURES],int,int,int);
+void measurements(coord*,coord*,double*,int,int);
+void seedingParticels(pos [][L],coord*,coord*,coord*,int);
+void particelswalk(pos [][L],coord*,coord*,long int*,long int*,int);
 
 
 int main () {
@@ -50,15 +49,15 @@ int main () {
   double rho = RHO;
   steps T, i;
   int L2 = L*L;
-  int N = L2*rho;
+  pos N = L2*rho;
 
   #ifdef VERBOSE_MODE
-  printf("Particels: %d\n", N);
+  printf("Particels: %lld\n", N);
   #endif
   
-  double Drho[MEASURES];
+  //double Drho[MEASURES];
 
-  int site[L][L];
+  pos site[L][L];
 
   
   coord *initsite;
@@ -71,7 +70,7 @@ int main () {
   particels = (coord *) calloc(N+1, sizeof(coord));
   if (particels == NULL) errori(100);
   double *meanDR2;
-  meanDR2 = (double *) calloc(N, sizeof(double));
+  meanDR2 = (double *) calloc(MEASURES, sizeof(double));
   if (meanDR2 == NULL) errori(100);
 
   long int Dnarrow[L], Snarrow[L];
@@ -82,6 +81,7 @@ int main () {
   
     init(site);
     seedingParticels(site, particels, initsite, truesite, N);
+    check_diocane(site);
 
     #ifdef VERBOSE_MODE
       printf("Initialization termined, story:%llu\n", i+1);
@@ -89,7 +89,7 @@ int main () {
 
     for(T=0; T<TMAX; T++){
       particelswalk(site, particels, truesite, Dnarrow, Snarrow, N);
-      if ((T % WIDTH) == 0) measurements(truesite, initsite, meanDR2, MEASURES, i, N);
+      if ((T % WIDTH) == 0) measurements(truesite, initsite, meanDR2, T/WIDTH, N);
     }
     
     #ifdef VISUAL_MODE
@@ -105,42 +105,11 @@ int main () {
       /******Analisi Dati******/
 
 
-  #ifdef RAWDATA
-    printf("DEFINED RAWDATA: STARTING F_PRINTING THE RAW ERRORS FILE\n");
-    
-    FILE *output1;
-    output1 = fopen("errDR2.dat", "w");
-    fprintf(output1, "#RAW DATA --GAS RETICOLARE--\n#Story\tMesure\terrDR2\n");
-    
-    for(i=0; i<NSTORIES; i++){
-      for(T=0; T<MEASURES; T++){
-        fprintf(output1, "%llu\t%llu\t%lf\n", i+1, T+1, errDR2[i][T]);
-      }
-    }
-    
-    #ifdef DEBUG_MODE
-      printf("writing on outputX: OK\n");
-    #endif
-    
-    fclose(output1);
-    
-    #ifdef DEBUG_MODE
-      printf("outputX - CLOSED\n");
-    #endif
-
-  #endif
-
   free(initsite);
   free(truesite);
   free(particels);
   free(meanDR2);
-  #ifdef VERBOSE_MODE 
-    printf("initsite\n");
-    printf("truesite\n");
-    printf("particels\n");
-    printf("meanDR2\n");
-    printf("close output\n");
-  #endif
+
   
   #ifdef CRONO_MODE
     clock_t end = clock();
@@ -150,7 +119,7 @@ int main () {
   exit(0);
 }
 
-void init(int site[L][L]){
+void init(pos site[L][L]){
   
   int x, y;
   
@@ -161,15 +130,6 @@ void init(int site[L][L]){
   }
 }
 
-void initdouble(double errDR2[NSTORIES][MEASURES]){
-  int n, m; 
-
-  for(n=0; n<NSTORIES; n++){
-    for(m=0; m<MEASURES; m++){
-      errDR2[n][m]=0.;
-    }
-  }
-}
 
 
 void initNeighbor(long int Dnarrow[], long int Snarrow[]){
@@ -184,7 +144,7 @@ void initNeighbor(long int Dnarrow[], long int Snarrow[]){
   Snarrow[0] = L - 1; 
 }
 
-void seedingParticels(int site[L][L], coord particels[], coord initstate[], coord truesite[], int N){
+void seedingParticels(pos site[L][L], coord particels[], coord initstate[], coord truesite[], int N){
 
   int p, x, y;
 
@@ -202,6 +162,20 @@ void seedingParticels(int site[L][L], coord particels[], coord initstate[], coor
     truesite[p].y = y;
   }
 }
+
+void check_diocane(pos site[L][L]){
+  int x, y;
+  for(x=0; x<L; x++){
+    for(y=0; y<L; y++){
+      if (site[x][y] > RHO*L*L) {
+        printf("PORCACCIO IL DIO MALEDETTO INFAME BOIA\n");
+        exit(-1);
+      }
+    }
+  }
+}
+
+
 
 // void Sites2Particels(coord state[], int site[L][L], int N){
   
@@ -227,7 +201,7 @@ void seedingParticels(int site[L][L], coord particels[], coord initstate[], coor
 //***************************PARTICEL WALK FUNCTION***************************//
 ////////////////////////////////////////////////////////////////////////////////
 
-void particelswalk(int site[L][L], coord particels[], coord truesite[], long int Dnarrow[], long int Snarrow[], int N){
+void particelswalk(pos site[L][L], coord particels[], coord truesite[], long int Dnarrow[], long int Snarrow[], int N){
   
   int p, r;
   int x, y;
@@ -240,8 +214,8 @@ void particelswalk(int site[L][L], coord particels[], coord truesite[], long int
   y = particels[p].y;
   
   #ifdef VERBOSE_MODE
-  printf("random particel choosed: %d\trandom direction choosed: %d", p, r);
-  printf("\t:%d\tx:%d\ty:%d", site[x][y], x, y);
+  printf("p:%d\tdir:%d", p, r);
+  printf("\tsite:%lld\tx:%d\ty:%d", site[x][y], x, y);
   #endif
 
   //la direzione del movimento è in senso orario r: 0,1,2,3 -> Nord, Est, Sud, Ovest
@@ -274,7 +248,7 @@ void particelswalk(int site[L][L], coord particels[], coord truesite[], long int
   }
   #ifdef VERBOSE_MODE
   else {
-    printf(" 0");
+    printf("\t0");
   }
   printf("\n");
   #endif
@@ -286,16 +260,22 @@ void particelswalk(int site[L][L], coord particels[], coord truesite[], long int
 
 
 
-void measurements(coord truesite[], coord initsite[], double meanDR2[], double errDR2[NSTORIES][MEASURES], int t, int storia, int N){
-  int p;
-  for(p=1; p<=N; p++) {
-    errDR2[storia][t] += pow(( truesite[p].x - initsite[p].x ), 2) + pow(( truesite[p].y - initsite[p].y ), 2);
+void measurements(coord truesite[], coord initsite[], double meanDR2[], int t, int N){
+  int p=0;
+  
+  for(p=1; p<=N; p++) {  
+    meanDR2[t+1] += pow(( truesite[p].x - initsite[p].x ), 2) + pow(( truesite[p].y - initsite[p].y ), 2);
   }
-  errDR2[storia][t]/=(double)N;
-  meanDR2[t] += errDR2[storia][t];
 }
 
-void fprinter(int site[L][L]){
+
+
+
+
+
+
+
+void fprinter(pos site[L][L]){
 
   int x, y;
   FILE *output;
