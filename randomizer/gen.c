9ec504e38202ab33cee4a674956243e78448e0ef
Aris997 <riccardo.riva.997@gmail.com>
2017-12-08 11:44:52 +0100

    updates rtandom

--- randomizer/gen.c
+++ randomizer/gen.c
@@ -35,29 +35,28 @@ int main(int argc, char **argv){
   output1 = fopen("hist.dat", "w");
   output2 = fopen("planes.dat", "w");
 
-  if (argc > 1) chooser = atol(argv[1]);
+  if (argc > 1) chooser = atoi(argv[1]);
   else {
     chooser = 0;
-    printf("Insert (1) for Shift Register, (2) RANDU or (0) Minimal Standard (as default)\n");
   }
-  
-  if (chooser == 0 || chooser == 2) {
-    
+
+  printf("# Insert by command line\n# "
+         "(0) Minimal Standard (as default),\n# "
+         "(1) for Shift Register or\n# "
+         "(2) RANDU\n# "
+         "(3) rand()\n# "
+         "(4) lrand48()\n");  
+
+
+  /***********************************************************************************************/
+  if (chooser == 0) {
+
     //MINIMAL STANDARD
     a = 16807;
     b = 0;
     m = (1ULL << 31) - 1ULL;
 
 
-    if (chooser == 2) {
-      // RANDU
-      a = 65539;
-      b = 0;
-      m = (1ULL << 31);
-      printf("Randu\n");
-    }
-
-    
     r = seed % m; //Inizializzazione di I_0
 
     //GENERATORE GCL
@@ -68,15 +67,15 @@ int main(int argc, char **argv){
       sumr += number;
       sumr2 += number*number;
       //printf("%lf\n",number);
-    
+
       bin = (int)(number*NBIN);
       hist[bin]++;
-    
+
       if (i%3 == 0) x = number;
       else if (i%3 == 1) y = number;
       else if (i%3 == 2) {
-	z = number;
-	fprintf(output2, "%lf %lf %lf\n", x, y, z);
+        z = number;
+        fprintf(output2, "%lf %lf %lf\n", x, y, z);
       }
     }
 
@@ -87,68 +86,122 @@ int main(int argc, char **argv){
     }
   }
 
-  
+  /***********************************************************************************************/
+  //SHIFT REGISTER
   else if (chooser == 1) {
-      //SHIFT REGISTER
 
-      //INIZIALIZZAZIONE
+    addB = 31;
+    addC = 0;
 
-      addB = 31;
-      addC = 0;
+    for (i=0; i<1000; i++) lrand48(); 
 
-      for (i=0; i<1000; i++) lrand48(); 
+    for (i=0; i<SHIFT; i++) SR[i]=0;
 
-      for (i=0; i<SHIFT; i++) SR[i]=0;
-
-      for (i=0; i<SHIFT; i++){
-	for (j=0; j<8*sizeof(RANDOM); j++){
-	  number = (double)lrand48()/(double)RAND_MAX;
-	  if (number < 0.5) {
-	    THEBIT = 1;
-	  }
-	  else {
-	    THEBIT = 0;
-	  }
-	  SR[i] = SR[i] | (THEBIT << j);
-	}
-      }
-
-      //GENERAZIONE
-
-      for (i=0; i<NMAX; i++){
-	r = SR[addB] ^ SR[addC];
-
-	SR[addC] = r;
-	addC++;
-	addB++;
-	if (addB == SHIFT) addB = 0;
-	if (addC == SHIFT) addC = 0;
-
-	number = (double)r / (double)ULLONG_MAX;
-
-	sumr += number;
-	sumr2 += number*number;
-	//printf("%lf\n",number);
-
-	bin = (int)(number*NBIN);
-	hist[bin]++;
-
-	if (i%3 == 0) x = number;
-	else if (i%3 == 1) y = number;
-	else if (i%3 == 2) {
-	  z = number;
-	  fprintf(output2, "%lf %lf %lf\n", x, y, z);
-	}
-      }
-
-
-
-      //DATI PER ISTOGRAMMA
-      for(i=0; i<NBIN; i++){
-	fprintf(output1,"%ld %ld\n", i, hist[i]);
-	phi += hist[i]*hist[i];
+    for (i=0; i<SHIFT; i++){
+      for (j=0; j<8*sizeof(RANDOM); j++){
+   number = (double)lrand48()/(double)RAND_MAX;
+   if (number < 0.5) {
+     THEBIT = 1;
+   }
+   else {
+     THEBIT = 0;
+   }
+   SR[i] = SR[i] | (THEBIT << j);
       }
     }
+
+    //GENERAZIONE
+
+    for (i=0; i<NMAX; i++){
+      r = SR[addB] ^ SR[addC];
+
+      SR[addC] = r;
+      addC++;
+      addB++;
+      if (addB == SHIFT) addB = 0;
+      if (addC == SHIFT) addC = 0;
+
+      number = (double)r / (double)ULLONG_MAX;
+
+      sumr += number;
+      sumr2 += number*number;
+      //printf("%lf\n",number);
+
+      bin = (int)(number*NBIN);
+      hist[bin]++;
+
+      if (i%3 == 0) x = number;
+      else if (i%3 == 1) y = number;
+      else if (i%3 == 2) {
+   z = number;
+   fprintf(output2, "%lf %lf %lf\n", x, y, z);
+      }
+    }
+
+
+
+    //DATI PER ISTOGRAMMA
+    for(i=0; i<NBIN; i++){
+      fprintf(output1,"%ld %ld\n", i, hist[i]);
+      phi += hist[i]*hist[i];
+    }
+  }
+
+  /*************************************************************************************************/
+  //RANDU
+  if(chooser == 2){
+
+    printf("# Randu\n");
+
+    a = 65539;
+    b = 0;
+    m = (1ULL << 31);
+
+
+
+    r = seed % m; //Inizializzazione di I_0
+
+    //GENERATORE GCL
+
+    for (i=0; i<NMAX; i++){
+      r = (a*r + b) % m;
+      number = (double)r / (double)m;
+      sumr += number;
+      sumr2 += number*number;
+      //printf("%lf\n",number);
+
+      bin = (int)(number*NBIN);
+      hist[bin]++;
+
+      if (i%3 == 0) x = number;
+      else if (i%3 == 1) y = number;
+      else if (i%3 == 2) {
+        z = number;
+        fprintf(output2, "%lf %lf %lf\n", x, y, z);
+      }
+    }
+
+    //DATI PER ISTOGRAMMA
+    for(i=0; i<NBIN; i++){
+      fprintf(output1,"%ld %ld\n", i, hist[i]);
+      phi += hist[i]*hist[i];
+    }
+  }
+
+  /*************************************************************************************************/
+
+  //rand()
+
+  if(chooser == 3){
+
+    //rand()
+
+
+
+
+  }
+
+  
 
   //METODI STATISTICI
   media = sumr/(double)NMAX;
@@ -156,21 +209,19 @@ int main(int argc, char **argv){
   chi = (double)phi * (double)NBIN/(double)NMAX - (double)NMAX;
 
 
-  printf("media: %lf varianza: %lf chi su %d GDL: %lf\n", media, varianza, NBIN-1, chi);
+  printf("# media: %lf varianza: %lf chi su %d GDL: %lf\n", media, varianza, NBIN-1, chi);
 
- 
+
 
 
   fclose(output1);
   fclose(output2);
- 
 
+#ifdef CRONO
   clock_t end = clock();
-  //printf("%.20lf\n %.20lf %ld\n",(double)end,(double)begin, CLOCKS_PER_SEC);
-
   double execution_time = (double)(end - begin)/CLOCKS_PER_SEC;
-  printf("Execution time: %lf\n", execution_time);
+  printf("# Execution time: %lf\n", execution_time);
+#endif
 
   exit(0);
-
 }
