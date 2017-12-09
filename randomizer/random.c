39bf86041852b714f057542045bea99ec73f0827
Aris997 <riccardo.riva.997@gmail.com>
2017-12-09 17:05:53 +0100

    manca solo la normale e accetta-rifiuta

--- randomizer/random.c
+++ randomizer/random.c
@@ -25,215 +25,224 @@ int main(){
 
   if(sizeof(ullint) < 8){ error(64); }
 
-  printf( "#\n# \tGenereratori di numeri casuali\n#\n"
+  printf( "#\n# \tGenereratori di numeri pseudocasuali\n#\n"
           "# In questo codice sono implementati:\n"
           "# (0) GCL Minimal Standard, (1) RANDU, (2) L'ecuyer [64bit], (3) rand(),\n"
-          "# (4) drand48(), (5) Shift-Register [ALL(?).]\n#\n"
+          "# (4) drand48(), (5) Shift-Register (6) TUTTI gli algoritmi\n#\n"
           "# Si applicano test statistici: media, varianza, CHI quadro\n"
           "# Box-Muller(numeri casuali distribuiti normalmente\n"
           "# Sceglire quale generatore analizzare: ");
 
-  int flag;
+  int flag, stories=1;
+
   do{
     scanf("%d", &flag);
-    if (flag < 0 || flag > 5) printf( "\n# (0) GCL, (1) RANDU, "
+    if (flag < 0 || flag > 6) printf( "\n# (0) GCL, (1) RANDU, "
                                       "(2) rand(), (3) drand48(), (4) Shift-Register\n"
           "# (5) L'ecuyer [64bit] [Per analizzarli tutti forse aggiorniamo.]\n"
           "Scegli meglio: ");
-  }while(flag < 0 || flag > 5);
+  }while(flag < 0 || flag > 6);
+
+  if(flag == 6) stories = 6;
 
   register ullint i;
 
-  double ran;
-  double sum=0, sum2=0;
-  double INV_RANDMAX = 1./RAND_MAX;
+  for(flag=0; flag<stories; flag++){ /////CICLO PER TUTTI GLI ALGORITMI/////////////////////////
 
-  lint *hist;
-  hist = (lint *)calloc(BIN, sizeof(lint));
-  if(hist == NULL){error(101);}
-  int bin;
+    double ran;
+    double sum=0, sum2=0;
+    double INV_RANDMAX = 1./RAND_MAX;
 
-/*********************************************************************************/
-  
-  if(flag <= 2){
+    lint *hist;
+    hist = (lint *)calloc(BIN, sizeof(lint));
+    if(hist == NULL){error(101);}
+    int bin;
 
-    double invM;
-
-    FILE *output1;
-    ullint m, a, seed;
-
-    if(flag == 0){  //GCL - MINIMAL STANDARD
-      
-      printf("# GCL - Minimal Standard\n");
-      m = (1ULL << 31) - 1ULL;
-      a = 16807;
-      seed = 756431;
-      invM = 1./(double)m;
-
-      output1 = fopen("minimalstd.dat", "w");
-      if (output1 == NULL){error(100);}
-    }
-
-    if(flag == 1){  //GCL - The infamous RANDU
-
-      printf("# GCL - RANDU\n");
-      m = (1ULL << 31);
-      a = 65539;
-      seed = time(NULL);
-      invM = 1./(double)m;
-
-      output1 = fopen("randu.dat", "w");
-      if (output1 == NULL){error(100);}
-    }
-
-    if(flag == 2){ //L'ecuyer type:1 -> 64bit
-
-      printf("#\n# L'Ecuyer type:1 64bit\n#\n");
-      m = ULLONG_MAX;//( 1 << 64 ) - 1;
-      a = 1181783497276652981ULL;
-      seed = time(NULL);
-      invM = 1./(double)m;
-
-      output1 = fopen("ecuyer.dat", "w");
-      if (output1 == NULL){error(100);}
-    }
-
-    ullint r = seed % m; //inizializzazione I_0
-
-    for(i=0; i<MAX; i++){
-      
-      r = ( a * r ) % m;    //il GCL
-      ran = (double)r * invM;
+  /*********************************************************************************/
     
-      bin = statistica(ran, &sum, &sum2, i, output1);
-      hist[bin]++;
-    }
-    fclose(output1);
-  }
+    if(flag <= 2){
 
-/*********************************************************************************/
+      double invM;
 
-  else if(flag <= 4){ // rand() e drand48()
+      FILE *output1;
+      ullint m, a, seed;
 
-
-    if(flag == 2){
-
-      printf("# rand()\n");
-
-      FILE *output2;
-      output2 = fopen("rand.dat", "w");
-      if(output2 == NULL){error(100);}
-
-      for(i=0; i<MAX; i++){
-
-        ran = rand()*INV_RANDMAX;
-        bin = statistica(ran, &sum, &sum2, i, output2);
-        hist[bin]++;
-
-      }
-      fclose(output2);
-    }
-
-    else if(flag == 3){
-
-      printf("# lrand48()\n");
-      FILE *output3;
-      output3 = fopen("lrand48.dat", "w");
-      if(output3 == NULL){error(100);}
-
-      for(i=0; i<MAX; i++){
-
-        ran = lrand48()*INV_RANDMAX;
-        bin = statistica(ran, &sum, &sum2, i, output3);
-        hist[bin]++;
-
-      }
-      fclose(output3);
-    }
-  }
-/*********************************************************************************/
-  else if(flag == 5){ //  SHIFT REGISTER
-
-    printf("# Shift-Register [inizializzato con lrand48()]\n");
-
-    int addB = 31;
-    int addC = 0;
-
-    int BITS = 8 * sizeof(ullint);
-
-    int k;
-
-    int THEBIT;
-
-    FILE *output5;
-    output5 = fopen("shiftregister.dat", "w");
-    if(output5 == NULL){error(100);}
-
-    ullint *SR, r;
-    SR = (ullint *) calloc(SHIFT, sizeof(ullint));
-    if(SR == NULL) {error(101);}
-
-    for(i=0; i<1000; i++) lrand48();
-
-    for(i=0; i<SHIFT; i++){
-      
-      for(k=0; k<BITS; k++){
+      if(flag == 0){  //GCL - MINIMAL STANDARD
         
-        ran = (double)lrand48()*INV_RANDMAX;
+        printf("#\n# GCL - Minimal Standard - FIXED SEED\n");
+        m = (1ULL << 31) - 1ULL;
+        a = 16807;
+        seed = 756431;
 
-        if(ran < 0.5) THEBIT = 1;
-        else THEBIT = 0;
+        output1 = fopen("minimalstd.dat", "w");
+        if (output1 == NULL){error(100);}
+      }
 
-        SR[i] = SR[i] | (THEBIT << k);
+      if(flag == 1){  //GCL - The infamous RANDU
+
+        printf("# GCL - RANDU\n");
+        m = (1ULL << 31);
+        a = 65539;
+        seed = time(NULL);
+
+        output1 = fopen("randu.dat", "w");
+        if (output1 == NULL){error(100);}
+      }
+
+      if(flag == 2){ //L'ecuyer type:1 -> 64bit
+
+        printf("# L'Ecuyer type:1 64bit\n");
+        m = ULLONG_MAX;//( 1 << 64 ) - 1;
+        a = 1181783497276652981ULL;
+        seed = time(NULL);
+
+        output1 = fopen("ecuyer.dat", "w");
+        if (output1 == NULL){error(100);}
+      }
+
+      invM = 1./(double)m;
+      ullint r = seed % m; //inizializzazione I_0
+
+      for(i=0; i<MAX; i++){
+        
+        r = ( a * r ) % m;    //il GCL
+        ran = (double)r * invM;
+      
+        bin = statistica(ran, &sum, &sum2, i, output1);
+        hist[bin]++;
+      }
+      fclose(output1);
+    }
+
+  /*********************************************************************************/
+
+    else if(flag <= 4){ // rand() e drand48()
+
+      if(flag == 3){
+
+        printf("# rand()\n");
+
+        FILE *output3;
+        output3 = fopen("rand.dat", "w");
+        if(output3 == NULL){error(100);}
+
+        for(i=0; i<MAX; i++){
+
+          ran = rand()*INV_RANDMAX;
+          bin = statistica(ran, &sum, &sum2, i, output3);
+          hist[bin]++;
+
+        }
+        fclose(output3);
+      }
+
+      else if(flag == 4){
+
+        printf("# lrand48()\n");
+        FILE *output4;
+        output4 = fopen("lrand48.dat", "w");
+        if(output4 == NULL){error(100);}
+
+        for(i=0; i<MAX; i++){
+
+          ran = lrand48()*INV_RANDMAX;
+          bin = statistica(ran, &sum, &sum2, i, output4);
+          hist[bin]++;
+        }
+        fclose(output4);
       }
     }
 
-    for(i=0; i<MAX; i++){
+  /*********************************************************************************/
+    else if(flag == 5){ //  SHIFT REGISTER
 
-      r = SR[addB] ^ SR[addC];
+      printf("# Shift-Register [inizializzato con lrand48()]\n");
 
-      SR[addC] = r;
-      addC++;
-      addB++;
+      int addB = 31;
+      int addC = 0;
 
-      if(addB == SHIFT) addB = 0;
-      if(addC == SHIFT) addC = 0;
-    
-      ran = (double)r/(double)ULLONG_MAX;
+      int BITS = 8 * sizeof(ullint);
 
-      bin = statistica(ran, &sum, &sum2, i, output5);
-      hist[bin]++;
+      int k;
+
+      int THEBIT;
+
+      FILE *output5;
+      output5 = fopen("shiftregister.dat", "w");
+      if(output5 == NULL){error(100);}
+
+      ullint *SR, r;
+      SR = (ullint *) calloc(SHIFT, sizeof(ullint));
+      if(SR == NULL) {error(101);}
+
+      for(i=0; i<1000; i++) lrand48(); //lrand48 gira 1000, inizializzandosi
+
+      for(i=0; i<SHIFT; i++){
+        
+        for(k=0; k<BITS; k++){
+          
+          ran = (double)lrand48()*INV_RANDMAX;
+
+          if(ran < 0.5) THEBIT = 1;
+          else THEBIT = 0;
+
+          SR[i] = SR[i] | (THEBIT << k);
+        }
+      }
+
+      for(i=0; i<MAX; i++){
+
+        r = SR[addB] ^ SR[addC];
+
+        SR[addC] = r;
+        addC++;
+        addB++;
+
+        if(addB == SHIFT) addB = 0;
+        if(addC == SHIFT) addC = 0;
+      
+        ran = (double)r/(double)ULLONG_MAX;
+
+        bin = statistica(ran, &sum, &sum2, i, output5);
+        hist[bin]++;
+      }
+      free(SR);
+      fclose(output5);
     }
-    free(SR);
-    fclose(output5);
-  }
 
-  //*****************************************************
-  //ISTOGRAMMA in outputH - 
-  FILE *outputH;
-  outputH = fopen("hist.dat", "w");
-  if(outputH == NULL){error(100);}
+    //*****************************************************
+    //ISTOGRAMMA in outputH - 
+   
+    FILE *outputH;
+    char filename[11];
+  
+    //vengono stampati istogrammi per ogni gen se si vogliono tutti
+    sprintf(filename, "hist%02d.dat", flag);
+    outputH = fopen(filename, "w");
+    if(outputH == NULL){ error(100); }
 
-  ullint phi;
+    ullint phi=0;
 
-  for(i=0; i<BIN; i++){
-    fprintf(outputH,"%lld %ld\n", i, hist[i]);
-    phi += hist[i]*hist[i];
-  }
+    for(i=0; i<BIN; i++){
+      fprintf(outputH,"%lld %ld\n", i, hist[i]);
+      phi += hist[i]*hist[i];
+    }
 
-  free(hist);
-  fclose(outputH);
-  //*****************************************************
+    free(hist);
+    fclose(outputH);
+    //*****************************************************
 
-  //Completamento della statistica
-  double media = sum/(double)MAX;
-  double varianza = (sum2 + (double)MAX * media * media - 2. * media * sum)/(double)(MAX - 1);
-  double chi = (double)phi * (double)BIN/(double)MAX - (double)MAX;
-  printf("media: %lf varianza: %lf chi su %u GDL: %lf\n", media, varianza, BIN-1, chi);
+    //Completamento della statistica
+    double media = sum/(double)MAX;
+    double varianza = (sum2 + (double)MAX * media * media - 2. * media * sum)/(double)(MAX - 1);
+    double chi = (double)phi * (double)BIN/(double)MAX - (double)MAX;
+    printf("# media: %lf varianza: %lf chi su %u GDL: %lf\n#\n", media, varianza, BIN-1, chi);
+
+  } ////////////////////////////////FINE CICLO ALGORITMI/////////////////////////////////
 
   #ifdef CRONO
     clock_t end = clock();
-    printf("Execution time:%lf\n", (double)(end-begin)/CLOCKS_PER_SEC);
+    printf("# Execution time:%lf\n", (double)(end-begin)/CLOCKS_PER_SEC);
   #endif    
 
   exit(0);
