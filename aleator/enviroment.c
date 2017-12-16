3cd8cb441fff75e1f141b0db55cf3c0f7c837d89
aris997 <riccardo.riva.997@gmail.com>
2017-12-16 16:15:33 +0100

    completed enviroment

--- aleator/enviroment.c
+++ aleator/enviroment.c
@@ -3,9 +3,9 @@
 #include <math.h>
 #include <time.h>
 
-#define BOOKS 1
-#define STORIES 1
-#define STEPS 1000
+#define BOOKS 100
+#define STORIES 100
+#define STEPS 10000
 #define GAP 10
 
 //RODARI-RIVA       labfc409      A.A.2017-2018         v0.1.0
@@ -23,16 +23,31 @@
 typedef unsigned long long int ullint;
 typedef long int lint;
 
+typedef struct meanbook{
+  double book;
+  double book2;
+}meanbook;
+
+
+
 void error(int);
 
 int main(){
 
   register ullint i;
-  srand(time(NULL));
+  srand(1234567);
 
   double *envr;
   envr = (double *) malloc( (2*STEPS + 1) * sizeof(double)); //Si inizializza un'array disparo.
-  if(envr == NULL) error(100);
+  if(envr == NULL) error(1);
+
+  meanbook *log;
+  log = (meanbook*) malloc( (STEPS) * sizeof(meanbook));
+  if(log == NULL) error(1);
+
+  meanbook *mean;
+  mean = (meanbook*) calloc(STEPS, sizeof(meanbook));
+  if(log == NULL) error(1);
 
   double r;
 
@@ -43,79 +58,129 @@ int main(){
   lint p;
 
   double INV_RANDMAX = 1./(double)RAND_MAX;
+  double invSTORY = 1./(double)STORIES;
+  double invBOOK = 1./(double)BOOKS;
+
 
   for(book = 0; book<BOOKS; book++){
 
     story = 0;
 
     envr = (double *) calloc( (2*STEPS + 1), sizeof(double));
-    if(envr == NULL) error(100);
+    if(envr == NULL) error(1);
 
+
+    //Inizializzo l'ambiente aleatorio con questo ciclo
+    //Le medie sulle storie vengono fatte per ogni libro
     for(i=0; i<(2*STEPS + 1); i++){
 
       r = (double)rand()*INV_RANDMAX;
       envr[i] = r;
-
+      
+      #ifdef DEBUG
+        fprintf(stderr, "%llu %lf\n", i, r);
+      #endif
 
     }
+
+
+    log = (meanbook*) calloc(STEPS, sizeof(meanbook));
+    if(log == NULL) error(1);
+    for(i=0; i<STEPS; i++) {log[i].book = 0.; log[i].book2 = 0.; }
+
 
 
     for(story = 0; story<STORIES; story++){
 
       p = 0;
+      #ifdef RAW_MODE
+        char filename[12];
+        //Creo il file con sistema automatico di denominazione
+        sprintf(filename, "story%02ld.dat", story+1);
+        FILE *output;
+        output = fopen(filename, "w");
+        if(output == NULL) error(2);
 
-      char filename[12];
-      sprintf(filename, "story%02ld.dat", story+1); //Creo il file con sistema automatico di denominazione
-      FILE *output;
-      output = fopen(filename, "w");
-      if(output == NULL) error(99);
-
-      fprintf(output, "#step position\n0 0\n");
-
+        fprintf(output, "#step position\n0 0\n");
+      #endif
 
       for(step = 0; step<STEPS; step++){
 
-
-        r = rand()*INV_RANDMAX;
+        r = (double)rand()*INV_RANDMAX;
 
         if ( p > 0){
           if(r < envr[2*p-1]) p++;
-          else if(r > envr[2*p-1]) p--;
+          else if(r >= envr[2*p-1]) p--;
+          else error(3);
         }
         
         else if(p < 0){
-          if(r < envr[-2*p]) p--;
-          else if(r > envr[-2*p]) p--;
+          if(r < envr[-2*p]) p++;
+          else if(r >= envr[-2*p]) p--;
+          else error(3);
         }
         
         else if(p == 0){
           if(r < envr[0]) p++;
-          else if(r > envr[0]) p--;
+          else if(r >= envr[0]) p--;
+          else error(3);
         }
 
-        fprintf(output, "%ld %ld\n", step + 1, p);
+        else error(4);
 
+        #ifdef RAW_MODE
+          fprintf(output, "%ld %ld\n", step + 1, p);
+        #endif
+        
+        log[step].book += (double)p;
+        log[step].book2 += (double)p*p;
 
       }
+      #ifdef RAW_MODE
+        fclose(output);
+      #endif
+    }
 
-      fclose(output);
 
+
+    for(step=0; step<STEPS; step++){
+      mean[step].book += log[step].book * invSTORY; 
+      mean[step].book2 += log[step].book2 * invSTORY;
     }
 
   }
 
-  free(envr);
 
+    for(step=0; step<STEPS; step++){
+      mean[step].book *= invBOOK;
+      mean[step].book2 *= invBOOK;
+      fprintf(stdout, "%ld %ld %lf %lf\n", step, step*step, mean[step].book, mean[step].book2);
+    }
+
+
+  free(envr);
+  free(mean);
+  free(log);
 
   exit(0);
 }
 
 void error(int n){
-  if(n == 100){
-    printf("ERROR! ALLOCAZIONE MEMORIA NON AVVENUTA\n");
+  if(n == 1){
+    fprintf(stderr, "ERROR! ALLOCAZIONE MEMORIA NON AVVENUTA\n");
+    exit(-1);
   }
-  else if(n == 99){
-    printf("ERROR! FILE NON APERTO CORRETTAMENTE!\n");
+  else if(n == 2){
+    fprintf(stderr, "ERROR! FILE NON APERTO CORRETTAMENTE!\n");
+    exit(-1);
   }
-  exit(-1);
+  else if(n == 3){
+    fprintf(stderr, "ERROR: rand number [0,1] didn't work\n");
+    exit(-1);
+  }
+  else if(n == 4){
+    fprintf(stderr, "ERROR: position is out o range\n");
+    exit(-1);
+  }
+  else exit(-1);
 }
